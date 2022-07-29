013d4d8a5004c7d5ecac49eaa64ff08346c347fb
Izabella Loyse Cândido <izabellaloyse13@gmail.com>
2022-07-28 22:27:42 -0300

    fix(search): adjusted layout file name

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -5,7 +5,7 @@ import Loading from '../../components/Loading/Loading';
 import SearchMovie from '../../components/SearchMovie/SearchMovie';
 import useFetch from '../../hooks/useFetch';
 import { IMovie } from '../../interfaces/movie';
-import Layout from '../../Layout/Layout';
+import Layout from '../../layout/Layout';
 import { ResultSearch, TitleResults } from './style';
 
 function Search() {
